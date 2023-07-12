bb6bd75a235835b836d9b544ca160c74ed906f0a
mohit <mohitrangani100@gmail.com>
2023-07-12 16:28:50 +0530

    second

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,3 +1,4 @@
+import 'package:account_manager/common/common_widget.dart';
 import 'package:account_manager/constant/images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,7 +14,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    var statusheight = MediaQuery.of(context).viewPadding.top;
+    var statusheight = MediaQuery
+        .of(context)
+        .viewPadding
+        .top;
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashbord"),
@@ -22,83 +26,7 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.deepPurple,
       ),
-      drawer: Drawer(
-        width: 300,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                  width: double.infinity,
-                  height: 310,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          fit: BoxFit.fill, image: AssetImage(sideimage))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(children: [
-                      statusheight.heightBox,
-                      Container(
-                        width: 90,
-                        height: 90,
-                      ),
-                      Divider(
-                        thickness: 2,
-                        color: Colors.white,
-                      ),
-                      Container(
-                        height: 100,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Colors.deepPurple,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 5),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              5.heightBox,
-                              Text("Credit(+)",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 18)),
-                              5.heightBox,
-                              Text("Debit(-)",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 18)),
-                              Divider(
-                                thickness: 2,
-                                color: Colors.white,
-                              ),
-                              Text("Balance",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        ),
-                      )
-                    ]),
-                  )),
-              drawer_list(Icon(Icons.home),"Home"),
-              drawer_list(Icon(Icons.backup),"Backup"),
-              drawer_list(Icon(Icons.restore),"Restore"),
-              drawer_list(Icon(Icons.currency_exchange),"Change currency"),
-              drawer_list(Icon(Icons.change_circle_outlined),"Change password"),
-              drawer_list(Icon(Icons.security),"Change security question"),
-              drawer_list(Icon(Icons.settings),"Settings"),
-              drawer_list(Icon(Icons.share),"Share app"),
-              drawer_list(Icon(Icons.star),"Rate app"),
-              drawer_list(Icon(Icons.privacy_tip_outlined),"Parivacy policy"),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-  Widget drawer_list(Icon drawer_icon, String name){
-    return ListTile(
-      leading: drawer_icon,
-      title: Text("$name"),
+      drawer: drawer(),
     );
   }
 }
