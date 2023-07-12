bce0cf2f283de9c1bc03a1055e1dbbaec0180d2c
mohit <mohitrangani100@gmail.com>
2023-07-12 17:36:41 +0530

    second

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,6 +1,7 @@
 import 'package:account_manager/common/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,10 +13,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    var statusheight = MediaQuery
-        .of(context)
-        .viewPadding
-        .top;
+    var statusheight = MediaQuery.of(context).viewPadding.top;
     return Scaffold(
       appBar: AppBar(
         title: Text("Dashbord"),
@@ -23,9 +21,100 @@ class _HomeState extends State<Home> {
           statusBarColor: Colors.deepPurpleAccent,
         ),
         backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert_outlined)),
+        ],
       ),
       drawer: drawer(),
+      body: Column(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return SimpleDialog(
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        color: Colors.deepPurple,
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Add new account",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.deepOrange),
+                          decoration: InputDecoration(
+                            hintText: "Account name",
+                            labelText: "Account name",
+                            labelStyle: TextStyle(color: Colors.deepOrange)
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          5.widthBox,
+                          Expanded(
+                              child: SizedBox(
+                                height: 40,
+                                child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ButtonStyle(
+                                        foregroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                Colors.deepPurple),
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                Colors.white),
+                                        shape: MaterialStateProperty.all<
+                                                RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20),
+                                                side: BorderSide(
+                                                    color: Colors.deepPurple)))),
+                                    child: Text("CANCEL")),
+                              )),
+                          5.widthBox,
+                          Expanded(
+                              child: SizedBox(
+                                height: 40,
+                                child: ElevatedButton(
+                                    onPressed: () {}, style: ButtonStyle(
+                                    foregroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.white),
+                                    backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.deepPurple),
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(20),
+                                            side: BorderSide(
+                                                color: Colors.deepPurple)))), child: Text("SAVE")),
+                              )),
+                          5.widthBox,
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add_to_photos_outlined),
+      ),
     );
   }
 }
-
